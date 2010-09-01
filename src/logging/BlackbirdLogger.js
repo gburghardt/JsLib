53be002298a5c2f1fe85fd8f55100ba80d9e519f
gburghardt <greg_burghardt@yahoo.com>
2010-09-01 18:10:34 -0500

    Adding a property to BlackbirdLogger identifying the blackbird logging object inside the window object.

--- src/logging/BlackbirdLogger.js
+++ src/logging/BlackbirdLogger.js
@@ -18,4 +18,14 @@ BlackbirdLogger.prototype = new BlackbirdLogger.prototype;
  */
 BlackbirdLogger.prototype.constructor = function( logHandle, level, debugMode, enabled ) {
 	BlackbirdLogger.superClass.constructor.call( this, logHandle, level, debugMode, enabled );
+	
+	this.setConsole( window[ BlackbirdLogger.namespace ] );
 };
+
+/**
+ * @static {String} Set this to the value of the NAMESPACE variable declared at
+ *                  the top of your blackbird.js file. By default, it is "log".
+ *                  This identifies the property in the window object where the
+ *                  blackbird console object resides.
+ */
+BlackbirdLogger.namespace = "log";
