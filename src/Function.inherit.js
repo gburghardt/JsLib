c20d4c591256859be380b26d300179e80a0ab0dd
gburghardt <greg_burghardt@yahoo.com>
2010-09-19 14:00:29 -0500

    Fleshing out the Function.inherit method.

--- src/Function.inherit.js
+++ src/Function.inherit.js
@@ -1,17 +1,56 @@
-Object.extend = function( parentClass, newProto ) {
+Function.inherit = function( parentClass, newProto ) {
+	var newConstructor = null;
 	try {
-		Object.extend.native( parentClass, newProto );
+		newConstructor = Function.inherit.native( parentClass, newProto );
+		Function.inherit = Function.inherit.native;
 	}
+	catch ( err ) {
+		newConstructor = Function.inherit.merge( parentClass, newProto );
+		Function.inherit = Function.inherit.merge;
+	}
+	
+	parentClass = null;
+	newProto = null;
+	
+	return newConstructor;
 };
 
-Object.extend.native = function( parentClass, newProto ) {
+Function.inherit.native = function( parentClass, newProto ) {
 	var newConstructor = function() {
 		this.constructor.apply( this, arguments );
 	};
 	
+	newConstructor.superClass = parentClass.prototype;
 	newConstructor.prototype = newProto;
 	newConstructor.prototype.__proto__ = parentClass.prototype;
 	
 	parentClass = null;
 	newProto = null;
+	
+	return newConstructor;
+};
+
+Function.inherit.merge = function( parentClass, newProto ) {
+	var undef;
+	
+	var newConstructor = function() {
+		this.constructor.apply( this, arguments );
+	};
+	
+	newConstructor.superClass = parentClass.prototype;
+	
+	for ( var key in parentClass.prototype ) {
+		if ( !parentClass.prototype.hasOwnProperty( key ) ) {
+			continue;
+		}
+		
+		if ( newProto[ key ] === undef ) {
+			newProto[ key ] = parentClass.prototype[ key ];
+		}
+	}
+	
+	newProto = null;
+	parentClass = null;
+	
+	return newConstructor;
 };
