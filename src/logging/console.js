730cee99e0c036afae6f1faaad7205a94d1b26b1
gburghardt <greg_burghardt@yahoo.com>
2010-09-22 10:28:14 -0500

    Removing dependency on jQuery for console.js

--- src/logging/console.js
+++ src/logging/console.js
@@ -5,9 +5,9 @@
 	if ( enableConsoleHack && typeof console !== "object" ) {
 		console = {
 			messages: [],
-		
+			
 			node: null,
-		
+			
 			init: function() {
 				this.node = document.createElement( "pre" );
 				this.node.style.backgroundColor = "#000";
@@ -17,40 +17,40 @@
 				this.node.style.overflow = "scroll";
 				this.node.style.maxWidth = "900px";
 				this.node.style.height = "600px";
-			
+				
 				document.getElementsByTagName( "body" )[ 0 ].appendChild( this.node );
-			
+				
 				this.displayCachedMessages();
 			},
-		
+			
 			appendMessage: function( message ) {
 				this.node.innerHTML += message + "<br /><br />";
 			},
-		
+			
 			displayCachedMessages: function() {
 				for ( var i = 0, length = this.messages.length; i < length; i++ ) {
 					this.appendMessage( this.messages[ i ] );
 				}
-			
+				
 				this.messages = [];
 			},
-
+			
 			info: function( message ) {
 				this.log( this.pad( "INFO", 5 ) + " " + message );
 			},
-
+			
 			debug: function( message ) {
 				this.log( this.pad( "DEBUG", 5 ) + " " + message );
 			},
-
+			
 			warn: function( message ) {
 				this.log( this.pad( "WARN", 5 ) + " " + message );
 			},
-
+			
 			error: function( message ) {
 				this.log( this.pad( "ERROR", 5 ) + " " + message );
 			},
-		
+			
 			log: function( message ) {
 				if ( !this.node ) {
 					this.messages.push( message );
@@ -59,21 +59,24 @@
 					this.appendMessage( message );
 				}
 			},
-		
+			
 			pad: function( str, len ) {
 				var diff = len - str.length;
 			
 				for ( var i = 0, length = diff; i < length; i++ ) {
 					str = " " + str;
 				}
-			
+				
 				return str;
 			}
 		};
-	
-		jQuery( function() {
+		
+		var oldOnload = window.onload || function() {};
+		
+		window.onload = function() {
+			oldOnload();
 			console.init();
-		} );
+		};
 	}
 	
 } )();
