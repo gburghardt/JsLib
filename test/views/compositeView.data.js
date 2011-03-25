161dfea395ba01a647f3b678c79e7e031d907a5a
gburghardt <greg_burghardt@yahoo.com>
2011-03-25 09:41:53 -0500

    Making composite template test/demo easier to read.

---
+++ test/views/compositeView.data.js
@@ -0,0 +1,932 @@
+function random() {
+	return Math.round(Math.random() * 100000);
+}
+var data = {
+	name: "John Doe",
+	age: 42,
+	"position.title": "CEO",
+	"position.salary": 100000,
+	id: "1234",
+	projects: [{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zodiac",
+		cost: random()
+	},{
+		name: "Zulu Ramp Up",
+		cost: 1009000
+	}],
+	contact: {
+		phone: "555-555-5555",
+		email: "jdoe@example.com",
+		address1: "555 E 33rd St",
+		address2: "Suite 85",
+		city: "Chicago",
+		region: "IL",
+		postalCode: "60601"
+	}
+};
