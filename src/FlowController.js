65779359b3e35657389f91ceaa71f536b5f301a8
gburghardt <gburghardt@tribune.com>
2011-11-13 07:51:25 -0600

    Adding pDoc comments.

--- src/FlowController.js
+++ src/FlowController.js
@@ -1,19 +1,43 @@
+/**
+ * class FlowController
+ *
+ * This class encapsulates the logic of switching between multiple forms.
+ **/
 function FlowController() {
 	this.constructor.apply(this, arguments);
 }
 
 FlowController.prototype = {
 
+	/**
+	 * FlowController#activeController -> ViewController
+	 **/
 	activeController: null,
 
+	/**
+	 * FlowController#controllers -> Object
+	 **/
 	controllers: null,
 
+	/**
+	 * FlowController#options -> Object
+	 **/
 	options: null,
 
+	/**
+	 * FlowController#rootNode -> HTMLElement
+	 **/
 	rootNode: null,
 
+	/**
+	 * FlowController#visible -> Boolean
+	 **/
 	visible: false,
 
+	/**
+	 * new FlowController([options])
+	 * - options (Object): Config options
+	 **/
 	constructor: function(options) {
 		this.options = {
 			onFinish: function() {}
@@ -23,6 +47,10 @@ FlowController.prototype = {
 		options = null;
 	},
 
+	/**
+	 * FlowController#init(rootNode)
+	 * - rootNode (HTMLElement): DOM node in which this flow controller lives
+	 **/
 	init: function(rootNode) {
 		this.rootNode = (typeof rootNode === "string") ? document.getElementById(rootNode) : rootNode;
 		this.createControllers();
@@ -30,6 +58,11 @@ FlowController.prototype = {
 		rootNode = null;
 	},
 
+	/**
+	 * FlowController#initControllers()
+	 * 
+	 * Initialize all view controllers
+	 **/
 	initControllers: function() {
 		for (var name in this.controllers) {
 			if (this.controllers.hasOwnProperty(name)) {
@@ -53,6 +86,12 @@ FlowController.prototype = {
 		this.activeController = this.options = null;
 	},
 
+	/**
+	 * FlowController#beforeClose() -> Undefined | Boolean
+	 *
+	 * Called before this flow controller is closed and destroyed. Returning
+	 * false prevents the closing of this flow controller.
+	 **/
 	beforeClose: function() {
 		
 	},
@@ -90,10 +129,21 @@ FlowController.prototype = {
 		args = options = null;
 	},
 
+	/**
+	 * FlowController#createControllers()
+	 *
+	 * Abstract method so sub classes can create the view controllers needed for
+	 * this flow.
+	 **/
 	createControllers: function() {
 		throw new Error("Sub classes of FlowController must implement a createControllers() method.");
 	},
 
+	/**
+	 * FlowController#finish(data)
+	 * - data (Object): Data from the view controllers passed to the onFinish
+	 *                  handler.
+	 **/
 	finish: function(data) {
 		this.options.onFinish(data);
 		this.close();
@@ -105,6 +155,15 @@ FlowController.prototype = {
 		}
 	},
 
+	/**
+	 * FlowController#getNextActiveController(name, action[, data]) -> ViewController | null
+	 * - name (String): Name of the controller ceding control
+	 * - action (String): Name of the last action performed on the controller
+	 * - data (Object): The data from the controller ceding control
+	 *
+	 * Get the controller that should be activated next after an action has been
+	 * performed. Sub classes must implement their own version of this method.
+	 **/
 	getNextActiveController: function(name, action, data) {
 		throw new Error("Sub classes of FlowController must implement a getNextActiveController() method.");
 	},
@@ -120,6 +179,18 @@ FlowController.prototype = {
 		}
 	},
 
+	/**
+	 * FlowController#processAction(name, action[, data])
+	 * - name (String): Name of the controller ceding control
+	 * - action (String): Name of the last action performed on the controller
+	 * - data (Object): The data from the controller ceding control
+	 *
+	 * Process an action performed in this flow where one controller is ceding
+	 * control to another controller. If no next controller is found, an error
+	 * is thrown.
+	 *
+	 * throws Error
+	 **/
 	processAction: function(name, action, data) {
 		var controller = this.getNextActiveController(name, action, data);
 
