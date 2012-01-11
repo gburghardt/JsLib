a43dbcc58b39787fd88c3b262d8026e6c5c5a27b
gburghardt <greg_burghardt@yahoo.com>
2012-01-11 09:23:40 -0600

    Changes to Delegator to allow namespacing and also bug fixing

--- src/framework/lib/dom/events/Delegator.js
+++ src/framework/lib/dom/events/Delegator.js
@@ -1,34 +1,83 @@
 window.dom = window.dom || {};
 window.dom.events = window.dom.events || {};
 
-dom.events.Delegator = function(node, types, delegate) {
-	this.node = node;
-	this.types = types = types.split(/[\s,]+/g);
-	this.delegate = delegate;
+dom.events.Delegator = function() {
 
-	this.destructor = function() {
-		var i = 0, length = types.length;
-		
-		for (i; i < length; ++i) {
-			node.removeEventListener(types[i], handleEvent, false);
+// Access: Public
+
+	this.constructor = function(delegate, node, actionPrefix) {
+		this.delegate = delegate;
+		this.node = node;
+		this.actionPrefix = actionPrefix || "";
+
+		if (!this.actionPrefix.match(/\.$/)) {
+			this.actionPrefix += ".";
 		}
 
-		node = this.node = delegate = this.delegate = null;
+		this.actionRegex = new RegExp("^" + this.actionPrefix.replace(/\./g, '\\.'));
+		this.eventTypes = [];
+		this.eventTypesAdded = {};
+	};
+
+	this.destructor = function() {
+		if (this.node) {
+			this.removeEventTypes(this.eventTypes);
+			this.node = null;
+		}
+
+		this.delegate = self = null;
 	};
 
 	this.init = function() {
-		var i = 0, length = types.length;
-
-		if (typeof node === "string") {
-			node = document.getElementById(node);
-		}
-
-		for (i; i < length; ++i) {
-			node.addEventListener(types[i], handleEvent, false);
+		if (typeof this.node === "string") {
+			this.node = document.getElementById(this.node);
 		}
 
 		return this;
 	};
+
+	this.addEventType = function(eventType) {
+		if (!this.eventTypesAdded[eventType]) {
+			this.eventTypes.push(eventType);
+			this.node.addEventListener(eventType, handleEvent, false);
+			this.eventTypesAdded[eventType] = true;
+		}
+	};
+
+	this.addEventTypes = function(eventTypes) {
+		var i = 0, length = eventTypes.length;
+
+		for (i; i < length; ++i) {
+			this.addEventType(eventTypes[i]);
+		}
+	};
+
+	this.removeEventType = function(eventType) {
+		if (this.eventTypesAdded[eventType]) {
+			this.node.removeEventListener(eventType, handleEvent, false);
+			this.eventTypesAdded[eventType] = false;
+		}
+	};
+
+	this.removeEventTypes = function(eventTypes) {
+		var i = 0, length = eventTypes.length;
+
+		for (i; i < length; ++i) {
+			this.removeEventType(eventTypes[i]);
+		}
+	};
+
+// Access: Private
+
+	var self = this;
+
+	this.node = null;
+
+	this.eventTypes = null;
+
+	this.eventTypesAdded = null;
+
+	this.delegate = null;
 
 	function stopPropagationPatch() {
 		this._stopPropagation();
@@ -51,24 +100,28 @@ dom.events.Delegator = function(node, types, delegate) {
 		if (event.actionTarget.getAttribute) {
 			// DOM node
 			action = event.actionTarget.getAttribute("data-action-" + event.type) ||
-			         event.actionTarget.getAttribute("data-action");
+							 event.actionTarget.getAttribute("data-action");
 		}
 		else if (event.actionTarget.documentURI) {
 			// document object
 			action = event.actionTarget["data-action-" + event.type] ||
-			         event.actionTarget["data-action"];
+							 event.actionTarget["data-action"];
 		}
 
-		if (action && delegate[action]) {
+		if (action) {
+			action = action.replace(self.actionRegex, "");
+		}
+
+		if (action && self.delegate[action]) {
 			try {
-				delegate[action](event, event.actionTarget);
+				self.delegate[action](event, event.actionTarget);
 			}
 			catch (error) {
 				event.preventDefault();
 				event.stopPropagation();
 
-				if (delegate.onActionError) {
-					delegate.onActionError(event, event.actionTarget, action, error);
+				if (self.delegate.onActionError) {
+					self.delegate.onActionError(event, event.actionTarget, action, error);
 				}
 				else if (window.console && window.console.error) {
 					window.console.error(error);
@@ -81,12 +134,13 @@ dom.events.Delegator = function(node, types, delegate) {
 			}
 		}
 
-		if (!event.propagationStopped && event.actionTarget !== node && event.actionTarget.parentNode) {
+		if (!event.propagationStopped && event.actionTarget !== self.node && event.actionTarget.parentNode) {
 			event.actionTarget = event.actionTarget.parentNode;
 			handleEvent(event);
 		}
 		else {
-			// Finished calling actions. Return event object to its normal state.
+			// Finished calling actions. Return event object to its normal state. Let
+			// event continue bubbling up the DOM.
 			event.actionTarget = null;
 			event.stopPropagation = event._stopPropagation;
 			event._stopPropagation = null;
@@ -96,4 +150,5 @@ dom.events.Delegator = function(node, types, delegate) {
 		event = null;
 	}
 
+	this.constructor.apply(this, arguments);
 };
