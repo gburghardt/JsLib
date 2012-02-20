6a30c213afbcf479904495951be24c4a69a2f1cb
gburghardt <greg_burghardt@yahoo.com>
2012-02-20 17:29:52 -0600

    Porting tests over to specs.

--- test/framework/models/BaseModel.spec.js
+++ test/framework/models/BaseModel.spec.js
@@ -23,6 +23,47 @@ describe("BaseModel", function() {
 		_validAttributes: ["firstName", "lastName"]
 	};
 
+	function TestValidation(attributes) {
+		this.constructor(attributes);
+	}
+	TestValidation.prototype = {
+		__proto__: BaseModel.prototype,
+		_validAttributes: [
+			"name",
+			"description",
+			"price",
+			"notes",
+			"phone"
+		],
+		requires: [
+			"price",
+			"name",
+			"description",
+			"notes",
+			"phone"
+		],
+		validatesNumeric: [
+			"price"
+		],
+		validatesMaxLength: {
+			name: 40,
+			description: 256
+		},
+		validatesFormatOf: {
+			name: /^testing.*$/,
+			phone: /^\s*\d{3}\s*[-.]*\s*\d{3}\s*[-.]*\s*\d{4}\s*$/
+		}
+	};
+
+	function TestNumericValidation(attributes) {
+		this.constructor(attributes);
+	}
+	TestNumericValidation.prototype = {
+		__proto__: BaseModel.prototype,
+		_validAttributes: ["price"],
+		validatesNumeric: ["price"]
+	};
+
 	it("defines a primary key by default", function() {
 		var o = new TestModel();
 		expect(o.isValidAttributeKey("id")).toEqual(true);
@@ -50,8 +91,8 @@ describe("BaseModel", function() {
 			expect(this.model.valueIsEmpty(foo)).toEqual(true);
 		});
 
-		it("returns true for NaN values", function() {
-			expect(this.model.valueIsEmpty(NaN)).toEqual(true);
+		it("returns false for NaN values", function() {
+			expect(this.model.valueIsEmpty(NaN)).toBeFalse();
 		});
 
 		it("returns true for empty strings", function() {
@@ -158,11 +199,139 @@ describe("BaseModel", function() {
 
 	});
 
-	describe("validation", function() {
+	describe("basicValidation module", function() {
 
-		describe("requires", function() {});
+		describe("validateRequiredAttributes", function() {
 
-		describe("numeric", function() {});
+			it("marks fields with 'empty'-like values as required", function() {
+				var o = new TestValidation({price: null, description: "", notes: "			"});
+				o.errors = {};
+				o.validateRequiredAttributes();
+
+				expect(o.hasErrors()).toBeTrue();
+				expect(o.errors.price).toBeArray();
+				expect(o.errors.price[0]).toEqual("is required");
+
+				expect(o.errors.name).toBeArray();
+				expect(o.errors.name[0]).toEqual("is required");
+
+				expect(o.errors.notes).toBeArray();
+				expect(o.errors.notes[0]).toEqual("is required");
+
+				expect(o.errors.description).toBeArray();
+				expect(o.errors.description[0]).toEqual("is required");
+
+				expect(o.errors.phone).toBeArray();
+				expect(o.errors.phone[0]).toEqual("is required");
+			});
+
+			it("validates when no attributes are passed in the constructor", function() {
+				var o = new TestValidation();
+				o.errors = {};
+				o.validateRequiredAttributes();
+
+				expect(o.hasErrors()).toBeTrue();
+				expect(o.errors.price).toBeArray();
+				expect(o.errors.price[0]).toEqual("is required");
+
+				expect(o.errors.name).toBeArray();
+				expect(o.errors.name[0]).toEqual("is required");
+
+				expect(o.errors.notes).toBeArray();
+				expect(o.errors.notes[0]).toEqual("is required");
+
+				expect(o.errors.description).toBeArray();
+				expect(o.errors.description[0]).toEqual("is required");
+
+				expect(o.errors.phone).toBeArray();
+				expect(o.errors.phone[0]).toEqual("is required");
+			});
+
+		});
+
+	});
+
+	describe("extendedValidation", function() {
+
+		describe("valueIsNumeric", function() {
+
+			beforeEach(function() {
+				this.model = new TestNumericValidation();
+			});
+
+			it("returns true for numbers", function() {
+				expect(this.model.valueIsNumeric(100)).toBeTrue();
+				expect(this.model.valueIsNumeric(-100)).toBeTrue();
+				expect(this.model.valueIsNumeric(0.3)).toBeTrue();
+			});
+
+			it("returns true for numeric strings", function() {
+				expect(this.model.valueIsNumeric("100")).toBeTrue();
+				expect(this.model.valueIsNumeric("-100")).toBeTrue();
+				expect(this.model.valueIsNumeric("0.3")).toBeTrue();
+			});
+
+			it("returns false for empty strings", function() {
+				expect(this.model.valueIsNumeric("")).toBeFalse();
+			});
+
+			it("returns false for null values", function() {
+				expect(this.model.valueIsNumeric(null)).toBeFalse();
+			});
+
+			it("returns false for NaN values", function() {
+				expect(this.model.valueIsNumeric(NaN)).toBeFalse();
+			});
+
+			it ("returns false for strings without numbers", function() {
+				expect(this.model.valueIsNumeric("abc")).toBeFalse();
+			});
+
+			it ("returns false for strings with numbers and non numeric characters", function() {
+				expect(this.model.valueIsNumeric("$10.05")).toBeFalse();
+			});
+
+		});
+
+		describe("numeric", function() {
+
+			beforeEach(function() {
+				this.model = new TestNumericValidation();
+				this.model.errors = {};
+				this.model.valid = true;
+			});
+
+			it("returns true for empty strings", function() {
+				this.model.price = "";
+				this.model.validateAttributeDataTypes();
+				expect(this.model.valid).toBeTrue();
+			});
+
+			it("returns true for null values", function() {
+				this.model.price = null;
+				this.model.validateAttributeDataTypes();
+				expect(this.model.valid).toBeTrue();
+			});
+
+			it("returns false for NaN values", function() {
+				this.model.price = NaN;
+				this.model.validateAttributeDataTypes();
+				expect(this.model.valid).toBeFalse();
+			});
+
+			it("returns false for non numeric strings", function() {
+				this.model.price = "acb";
+				this.model.validateAttributeDataTypes();
+				expect(this.model.valid).toBeFalse();
+			});
+
+			it("returns false for strings with numbers, but other non number characters", function() {
+				this.model.price = "$10.99";
+				this.model.validateAttributeDataTypes();
+				expect(this.model.valid).toBeFalse();
+			});
+
+		});
 
 		describe("max length", function() {});
 
@@ -176,6 +345,10 @@ describe("BaseModel", function() {
 
 	describe("toXML", function() {});
 
-	describe("getClassReference", function() {});
+	describe("relations", function() {
+
+		describe("getClassReference", function() {});
+
+	});
 
 });
