2425c8b1703c2bb2d11575227f4a2bc21f04c6ed
gburghardt <gburghardt@tribune.com>
2012-03-01 16:58:29 -0600

    Moving model class definitions out of describe block to make it easier to read.

--- test/framework/models/BaseModel.spec.js
+++ test/framework/models/BaseModel.spec.js
@@ -1,132 +1,132 @@
+
+function TestModel(attributes) {
+	this.constructor(attributes);
+}
+TestModel.prototype = {
+	__proto__: BaseModel.prototype
+};
+
+function TestModelPrimaryKeyOverride(attributes) {
+	this.constructor(attributes);
+}
+TestModelPrimaryKeyOverride.prototype = {
+	__proto__: BaseModel.prototype,
+	primaryKey: "foo_id"
+};
+
+function TestModelAttributes(attributes) {
+	this.constructor(attributes);
+}
+TestModelAttributes.prototype = {
+	__proto__: BaseModel.prototype,
+	_validAttributes: ["firstName", "lastName"]
+};
+
+function TestValidation(attributes) {
+	this.constructor(attributes);
+}
+TestValidation.prototype = {
+	__proto__: BaseModel.prototype,
+	_validAttributes: [
+		"name",
+		"description",
+		"price",
+		"notes",
+		"phone"
+	],
+	requires: [
+		"price",
+		"name",
+		"description",
+		"notes",
+		"phone"
+	],
+	validatesNumeric: [
+		"price"
+	],
+	validatesMaxLength: {
+		name: 40,
+		description: 256
+	},
+	validatesFormatOf: {
+		name: /^testing.*$/,
+		phone: /^\s*\d{3}\s*[-.]*\s*\d{3}\s*[-.]*\s*\d{4}\s*$/
+	}
+};
+
+function TestMaxLengthValidation(attributes) {
+	this.constructor(attributes);
+}
+TestMaxLengthValidation.prototype = {
+	__proto__: BaseModel.prototype,
+	_validAttributes: ["name", "description", "notes"],
+	validatesMaxLength: {name: 10, description: 8, notes: 4}
+};
+
+function TestNumericValidation(attributes) {
+	this.constructor(attributes);
+}
+TestNumericValidation.prototype = {
+	__proto__: BaseModel.prototype,
+	_validAttributes: ["price"],
+	validatesNumeric: ["price"]
+};
+
+function TestFormatValidation(attributes) {
+	this.constructor(attributes);
+}
+TestFormatValidation.prototype = {
+	__proto__: BaseModel.prototype,
+	_validAttributes: ["phone", "address"],
+	validatesFormatOf: {
+		phone: [
+			/^\d{3}-\d{3}-\d{4}$/,
+			/^\(\d{3}\) \d{3}-\d{4}$/
+		],
+		address: /^\d+ \w+( [.\w]+)?, \w+, [A-Z]{2} \d{5}$/
+	}
+};
+
+function TestRelations(attributes) {
+	this.constructor(attributes);
+}
+TestRelations.prototype = {
+	__proto__: BaseModel.prototype,
+	_validAttributes: ['name', 'description', 'price', 'quantity', 'category_id'],
+	hasOne: {
+		category: {className: 'Category'}
+	}
+};
+
+function Store(attributes) {
+	this.constructor(attributes);
+}
+Store.prototype = {
+	__proto__: BaseModel.prototype,
+	_validAttributes: ["id", "name"],
+	hasMany: {
+		deals: {className: "Deal", foreignKey: "store_id"}
+	}
+};
+
+function Category(attributes) {
+	this.constructor(attributes);
+}
+Category.prototype = {
+	__proto__: BaseModel.prototype,
+	_validAttributes: ['id', 'name']
+};
+
+function Deal(attributes) {
+	this.constructor(attributes);
+}
+Deal.prototype = {
+	__proto__: BaseModel.prototype,
+	_validAttributes: ['id', 'store_id', 'name', 'description', 'start_date', 'end_date'],
+	requires: ['id', 'store_id']
+};
+
 describe("BaseModel", function() {
-
-	function TestModel(attributes) {
-		this.constructor(attributes);
-	}
-	TestModel.prototype = {
-		__proto__: BaseModel.prototype
-	};
-
-	function TestModelPrimaryKeyOverride(attributes) {
-		this.constructor(attributes);
-	}
-	TestModelPrimaryKeyOverride.prototype = {
-		__proto__: BaseModel.prototype,
-		primaryKey: "foo_id"
-	};
-
-	function TestModelAttributes(attributes) {
-		this.constructor(attributes);
-	}
-	TestModelAttributes.prototype = {
-		__proto__: BaseModel.prototype,
-		_validAttributes: ["firstName", "lastName"]
-	};
-
-	function TestValidation(attributes) {
-		this.constructor(attributes);
-	}
-	TestValidation.prototype = {
-		__proto__: BaseModel.prototype,
-		_validAttributes: [
-			"name",
-			"description",
-			"price",
-			"notes",
-			"phone"
-		],
-		requires: [
-			"price",
-			"name",
-			"description",
-			"notes",
-			"phone"
-		],
-		validatesNumeric: [
-			"price"
-		],
-		validatesMaxLength: {
-			name: 40,
-			description: 256
-		},
-		validatesFormatOf: {
-			name: /^testing.*$/,
-			phone: /^\s*\d{3}\s*[-.]*\s*\d{3}\s*[-.]*\s*\d{4}\s*$/
-		}
-	};
-
-	function TestMaxLengthValidation(attributes) {
-		this.constructor(attributes);
-	}
-	TestMaxLengthValidation.prototype = {
-		__proto__: BaseModel.prototype,
-		_validAttributes: ["name", "description", "notes"],
-		validatesMaxLength: {name: 10, description: 8, notes: 4}
-	};
-
-	function TestNumericValidation(attributes) {
-		this.constructor(attributes);
-	}
-	TestNumericValidation.prototype = {
-		__proto__: BaseModel.prototype,
-		_validAttributes: ["price"],
-		validatesNumeric: ["price"]
-	};
-
-	function TestFormatValidation(attributes) {
-		this.constructor(attributes);
-	}
-	TestFormatValidation.prototype = {
-		__proto__: BaseModel.prototype,
-		_validAttributes: ["phone", "address"],
-		validatesFormatOf: {
-			phone: [
-				/^\d{3}-\d{3}-\d{4}$/,
-				/^\(\d{3}\) \d{3}-\d{4}$/
-			],
-			address: /^\d+ \w+( [.\w]+)?, \w+, [A-Z]{2} \d{5}$/
-		}
-	};
-
-	function TestRelations(attributes) {
-		this.constructor(attributes);
-	}
-	TestRelations.prototype = {
-		__proto__: BaseModel.prototype,
-		_validAttributes: ['name', 'description', 'price', 'quantity', 'category_id'],
-		hasOne: {
-			category: {className: 'Category'}
-		}
-	};
-
-	function Store(attributes) {
-		this.constructor(attributes);
-	}
-	Store.prototype = {
-		__proto__: BaseModel.prototype,
-		_validAttributes: ["id", "name"],
-		hasMany: {
-			deals: {className: "Deal", foreignKey: "store_id"}
-		}
-	};
-
-	function Category(attributes) {
-		this.constructor(attributes);
-	}
-	Category.prototype = {
-		__proto__: BaseModel.prototype,
-		_validAttributes: ['id', 'name']
-	};
-	window.Category = Category;
-
-	function Deal(attributes) {
-		this.constructor(attributes);
-	}
-	Deal.prototype = {
-		__proto__: BaseModel.prototype,
-		_validAttributes: ['id', 'store_id', 'name', 'description', 'start_date', 'end_date']
-	};
-	window.Deal = Deal;
 
 	it("defines a primary key by default", function() {
 		var o = new TestModel();
