baef133654560c7c7b39600adbd34dfd703cc575
Greg Burghardt <greg_burghardt@yahoo.com>
2012-08-07 17:22:41 -0500

    Spaces to Tabs

--- src/lib/patches/Function.js
+++ src/lib/patches/Function.js
@@ -1,14 +1,14 @@
 if (!Function.prototype.bind) {
-  Function.prototype.bind = function(context) {
-    var self = this;
-    var fn = function() {
-      return self.apply(context, arguments);
-    };
+	Function.prototype.bind = function(context) {
+		var self = this;
+		var fn = function() {
+			return self.apply(context, arguments);
+		};
 
-    fn.cleanup = function() {
-      self = fn = context = null;
-    };
+		fn.cleanup = function() {
+			self = fn = context = null;
+		};
 
-    return fn;
-  };
+		return fn;
+	};
 }
