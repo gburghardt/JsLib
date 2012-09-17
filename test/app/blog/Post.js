29105a342d4c4409b0c004548852085c89696db3
Greg Burghardt <greg_burghardt@yahoo.com>
2012-09-17 18:06:14 -0500

    Adding some validation to the test app.

--- test/app/blog/Post.js
+++ test/app/blog/Post.js
@@ -1,5 +1,12 @@
 blog.Post = BaseModel.extend({
 	prototype: {
-		validAttributes: ["title", "body", "publish_date"]
+		validAttributes: ["title", "body", "publish_date"],
+		requires: ["title"],
+		validatesFormatOf: {
+			publish_date: [
+				/^[0-9]{4}[-\/][0-9]{2}[-\/][0-9]{2}$/, // YYYY/MM/DD
+				/^[0-9]{2}[-\/][0-9]{2}[-\/][0-9]{4}$/  // MM/DD/YYYY
+			]
+		}
 	}
 });
