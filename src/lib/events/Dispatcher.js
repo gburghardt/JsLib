e4f6764918d23ce75ff4a569c70125fb3e1c1a80
Greg Burghardt <greg_burghardt@yahoo.com>
2013-02-09 17:36:46 -0600

    Using Object#extend to create events.Dispatcher class.

--- src/lib/events/Dispatcher.js
+++ src/lib/events/Dispatcher.js
@@ -1,104 +1,109 @@
 window.events = window.events || {};
 
-events.Dispatcher = function() {
-	this.subscribers = {};
-};
+events.Dispatcher = Object.extend({
 
-events.Dispatcher.logger = null;
+	self: {
+		logger: null
+	},
 
-events.Dispatcher.prototype = {
+	prototype: {
 
-	subscribers: null,
+		subscribers: null,
 
-	destructor: function() {
-		var type, i, length, subscribers;
+		initialize: function() {
+			this.subscribers = {};
+		},
 
-		if (this.subscribers) {
-			for (type in this.subscribers) {
-				if (this.subscribers.hasOwnProperty(type)) {
-					subscribers = this.subscribers[type];
+		destructor: function() {
+			var type, i, length, subscribers;
 
-					for (i = 0, length = subscribers.length; i < length; i++) {
-						subscribers[i] = null;
+			if (this.subscribers) {
+				for (type in this.subscribers) {
+					if (this.subscribers.hasOwnProperty(type)) {
+						subscribers = this.subscribers[type];
+
+						for (i = 0, length = subscribers.length; i < length; i++) {
+							subscribers[i] = null;
+						}
+
+						this.subscribers[type] = null;
 					}
+				}
 
-					this.subscribers[type] = null;
+				this.subscribers = null;
+			}
+		},
+
+		publish: function(type, publisher, data) {
+			if (!this.subscribers[type]) {
+				return false;
+			}
+
+			var event = new events.Event(type, publisher, data);
+			var subscribers = this.subscribers[type], i = 0, length = subscribers.length, subscriber;
+
+			for (i; i < length; i++) {
+				if (event.cancelled) {
+					break;
+				}
+
+				subscriber = subscribers[i];
+
+				try {
+					subscriber.instance[ subscriber.method ](event, event.data);
+				}
+				catch (error) {
+					if (events.Dispatcher.logger) {
+						events.Dispatcher.logger.error("events.Dispatcher#publish - An error was thrown while publishing event " + type);
+						events.Dispatcher.logger.error(error);
+					}
+					else {
+						event = publisher = data = subscribers = null;
+						throw error;
+					}
 				}
 			}
 
-			this.subscribers = null;
-		}
-	},
+			event = publisher = data = subscribers = null;
 
-	publish: function(type, publisher, data) {
-		if (!this.subscribers[type]) {
-			return false;
-		}
+			return true;
+		},
 
-		var event = new events.Event(type, publisher, data);
-		var subscribers = this.subscribers[type], i = 0, length = subscribers.length, subscriber;
+		subscribe: function(type, instance, method) {
+			this.subscribers[type] = this.subscribers[type] || [];
+			this.subscribers[type].push({instance: instance, method: method || "handleEvent"});
+			instance = null;
+		},
 
-		for (i; i < length; i++) {
-			if (event.cancelled) {
-				break;
-			}
-
-			subscriber = subscribers[i];
-
-			try {
-				subscriber.instance[ subscriber.method ](event, event.data);
-			}
-			catch (error) {
-				if (events.Dispatcher.logger) {
-					events.Dispatcher.logger.error("events.Dispatcher#publish - An error was thrown while publishing event " + type);
-					events.Dispatcher.logger.error(error);
-				}
-				else {
-					event = publisher = data = subscribers = null;
-					throw error;
-				}
-			}
-		}
-
-		event = publisher = data = subscribers = null;
-
-		return true;
-	},
-
-	subscribe: function(type, instance, method) {
-		this.subscribers[type] = this.subscribers[type] || [];
-		this.subscribers[type].push({instance: instance, method: method || "handleEvent"});
-		instance = null;
-	},
-
-	unsubscribe: function(type, instance) {
-		if (this.subscribers[type]) {
-			var subscribers = this.subscribers[type], i = subscribers.length;
-
-			while (i--) {
-				if (subscribers[i].instance === instance) {
-					subscribers.splice(i, 1);
-				}
-			}
-
-			subscribers = instance = null;
-		}
-	},
-
-	unsubscribeAll: function(instance) {
-		var type, i;
-
-		for (type in this.subscribers) {
-			if (this.subscribers.hasOwnProperty(type)) {
-				i = this.subscribers[type].length;
+		unsubscribe: function(type, instance) {
+			if (this.subscribers[type]) {
+				var subscribers = this.subscribers[type], i = subscribers.length;
 
 				while (i--) {
-					if (this.subscribers[type][i].instance === instance) {
-						this.subscribers[type].splice(i, 1);
+					if (subscribers[i].instance === instance) {
+						subscribers.splice(i, 1);
+					}
+				}
+
+				subscribers = instance = null;
+			}
+		},
+
+		unsubscribeAll: function(instance) {
+			var type, i;
+
+			for (type in this.subscribers) {
+				if (this.subscribers.hasOwnProperty(type)) {
+					i = this.subscribers[type].length;
+
+					while (i--) {
+						if (this.subscribers[type][i].instance === instance) {
+							this.subscribers[type].splice(i, 1);
+						}
 					}
 				}
 			}
 		}
 	}
 
-};
+});
