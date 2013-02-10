151f6dd335d652c2e2b2dee5797eaac8e7893294
Greg Burghardt <greg_burghardt@yahoo.com>
2013-02-10 10:08:11 -0600

    The HTMLElement class is a unique beast. It's an object with a prototype, not a function with a prototype. We fudge the include() method call and create HTMLElement#include as a cludge for other adaptors.

--- src/lib/HTMLElement/Adaptors.js
+++ src/lib/HTMLElement/Adaptors.js
@@ -1,9 +1,27 @@
-HTMLElement.Adaptors = {
-	prototype: {
-		querySelector: document.querySelector = function(selector) {
-			return this.querySelectorAll(selector)[0] || null;
-		}
-	}
-};
+(function() {
+	var idIndex = 0;
 
-HTMLElement.include(HTMLElement.Adaptors);
+	HTMLElement.Adaptors = {
+		self: {
+			include: function(mixin) {
+				Function.prototype.include.call(this, mixin);
+			}
+		},
+		prototype: {
+			identify: function() {
+				if (!this.id) {
+					this.id = this.getAttribute("id") || 'anonoymous-' + this.nodeName.toLowerCase() + '-' + (idIndex++);
+					this.setAttribute("id", this.id);
+				}
+
+				return this.id;
+			},
+			querySelector: document.querySelector = function(selector) {
+				return this.querySelectorAll(selector)[0] || null;
+			}
+		}
+	};
+})();
+
+// HTMLElement is a special object which is not instantiable, but has a prototype.
+Function.prototype.include.call(HTMLElement, HTMLElement.Adaptors);
