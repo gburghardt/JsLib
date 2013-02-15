b8fb0c2a5f88f86e1fa9a967bd8ee2c95adfabe6
Greg Burghardt <greg_burghardt@yahoo.com>
2013-02-15 08:26:14 -0600

    Adding BaseModule.ModuleTree mixin, but not sure if it's of any use now that I tried using it.

---
+++ src/lib/BaseModule/ModuleTree.js
@@ -0,0 +1,65 @@
+// TODO: Is this even useful? Keeping this file in the mean time, but not sure if I will long term.
+BaseModule.ModuleTree = {
+
+	included: function(Klass) {
+		Klass.attempt("addCallbacks", {
+			afterInit: "initModuleTree",
+			beforeDestructor: "destroyModuleTree"
+		});
+	},
+
+	prototype: {
+
+		childModules: {},
+
+		parentModule: null,
+
+		initModuleTree: function() {
+			if (!this.__proto__.compiledChildModules) {
+				var compiledChildModules = this.mergePropertyFromPrototypeChain("childModules");
+
+				this.__proto__.compiledChildModules = compiledChildModules;
+
+				for (var property in compiledChildModules) {
+					if (compiledChildModules.hasOwnProperty(property) && !this.__proto__.hasOwnProperty(property)) {
+						Object.defineProperty(this.__proto__, property, {
+							get: this.createChildModuleGetter(property)
+						});
+					}
+				}
+
+			}
+		},
+
+		destroyModuleTree: function() {
+			// TODO: Destroy all humans--- modules. I mean modules!
+		},
+
+		createChildModuleGetter: function(property) {
+			if (this.compiledChildModules[property] === null) {
+				return function() {
+					if (!this.compiledChildModules[property]) {
+						var elements = this.element.getElementsByTagName("*");
+						this.compiledChildModules[property] = this.createModuleSingleProperty(property, elements);
+						elements = null;
+					}
+
+					return this.compiledChildModules[property];
+				};
+			}
+			else if (this.compiledChildModules[property] instanceof Array) {
+				if (!this.compiledChildModules[property].length) {
+					var elements = this.element.getElementsByTagName("*");
+					this.compiledChildModules[property] = this.createModuleArrayProperty(property, elements);
+					elements = null;
+				}
+
+				return this.compiledChildModules[property];
+			}
+		}
+
+	}
+
+};
+
+BaseModule.include(BaseModule.ModuleTree);
