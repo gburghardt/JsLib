26fab8ee80e5b7171842d53c16c76cb52d7a2e6d
Greg Burghardt <greg_burghardt@yahoo.com>
2013-02-20 18:15:40 -0600

    Moving test/lib/BaseModel/Serialization/Json.spec.js -> src/lib/model/spec/model/serialization/json_spec.js

---
+++ src/lib/model/spec/model/serialization/json_spec.js
@@ -0,0 +1,183 @@
+describe("BaseModel", function() {
+
+	describe("Serialization", function() {
+
+		describe("toJson", function() {
+
+			beforeEach(function() {
+				this.model = new TestValidation({
+					id: 1234,
+					name: "Paint",
+					description: "Red<br>matte",
+					price: 15.99,
+					notes: "Per gallon",
+					phone: null
+				});
+			});
+
+			it("serializes attributes into an anonymous object", function() {
+				var json = [
+					'{',
+						'"name":"Paint",',
+						'"description":"Red<br>matte",',
+						'"price":15.99,',
+						'"notes":"Per gallon",',
+						'"phone":null,',
+						'"id":1234',
+					'}'
+				].join("");
+
+				expect(this.model.toJson()).toEqual(json);
+			});
+
+			it("serializes attributes into a named object", function() {
+				var json = [
+					'{',
+						'"test_validation":{',
+							'"name":"Paint",',
+							'"description":"Red<br>matte",',
+							'"price":15.99,',
+							'"notes":"Per gallon",',
+							'"phone":null,',
+							'"id":1234',
+						'}',
+					'}'
+				].join("");
+
+				expect(this.model.toJson({rootElement: "test_validation"})).toEqual(json);
+			});
+
+			it("serializes changed attributes", function() {
+				var json = [
+					'{',
+						'"name":"Stain",',
+						'"id":1234',
+					'}'
+				].join("");
+				this.model.name = "Stain";
+				expect(this.model.changedAttributes.name).toEqual("Paint");
+				expect(this.model.toJson({changedAttributesOnly: true})).toEqual(json);
+			});
+
+			it("serializes attributes into with a root element", function() {
+				this.model.name = "Ink";
+				var expected = '{"test_validation":{"name":"Ink","id":1234}}';
+				var actual = this.model.toJson({rootElement: "test_validation", changedAttributesOnly: true});
+				expect(actual).toEqual(expected);
+			});
+
+			it("serializes only the primary key when no changed attributes exist", function() {
+				var json = '{"id":1234}';
+				expect(this.model.toJson({changedAttributesOnly: true})).toEqual(json);
+			});
+
+			describe("default options", function() {
+
+				it("sets the format", function() {
+					var Klass = BaseModel.extend({
+						prototype: {
+							serializeOptions: { format: "json" },
+							validAttributes: ["name"]
+						}
+					});
+					var model = new Klass({name: "Testing", id: 123});
+					var expected = '{"name":"Testing","id":123}';
+					var actual = model.serialize();
+
+					expect(actual).toEqual(expected);
+				});
+
+				it("includes a root element", function() {
+					var Klass = BaseModel.extend({
+						prototype: {
+							serializeOptions: { format: "json", rootElement: "klass" },
+							validAttributes: ["name"]
+						}
+					});
+					var model = new Klass({name: "Testing", id: 123});
+					var expected = '{"klass":{"name":"Testing","id":123}}';
+					var actual = model.serialize();
+
+					expect(actual).toEqual(expected);
+				});
+
+				it("includes only changed attributes", function() {
+					var Klass = BaseModel.extend({
+						prototype: {
+							serializeOptions: { format: "json", changedAttributesOnly: true },
+							validAttributes: ["name", "description"]
+						}
+					});
+					var model = new Klass({name: "Testing", description: "foo", id: 123});
+					var expected = '{"name":"Testing changed","id":123}';
+					model.name = "Testing changed";
+					var actual = model.serialize();
+
+					expect(actual).toEqual(expected);
+				});
+
+				it("includes changed attributes, a root element and sets the format", function() {
+					var Klass = BaseModel.extend({
+						prototype: {
+							serializeOptions: { format: "json", changedAttributesOnly: true, rootElement: "klass" },
+							validAttributes: ["name", "description"]
+						}
+					});
+					var model = new Klass({name: "Testing", description: "foo", id: 123});
+					var expected = '{"klass":{"name":"Test Changed","id":123}}';
+					model.name = "Test Changed";
+					var actual = model.serialize();
+
+					expect(actual).toEqual(expected);
+				});
+
+				it("allows the root element to be overrided", function() {
+					var Klass = BaseModel.extend({
+						prototype: {
+							serializeOptions: { format: "json", rootElement: "wrong" }
+						}
+					});
+					var model = new Klass({id: 123});
+					var expected = '{"right":{"id":123}}';
+					var actual = model.serialize({rootElement: "right"});
+
+					expect(actual).toEqual(expected);
+				});
+
+				it("allows changed attributes only flag to be overrided", function() {
+					var Klass = BaseModel.extend({
+						prototype: {
+							serializeOptions: { format: "json", changedAttributesOnly: true },
+							validAttributes: ["name", "description"]
+						}
+					});
+					var model = new Klass({name: "Test", description: "Foo", id: 123});
+					model.description = "Foo changed";
+					var expected = '{"name":"Test","description":"Foo changed","id":123}';
+					var actual = model.serialize({changedAttributesOnly: false});
+
+					expect(actual).toEqual(expected);
+				});
+
+				it("allows all options to be overrided", function() {
+					var Klass = BaseModel.extend({
+						prototype: {
+							serializeOptions: { format: "json", changedAttributesOnly: true, rootElement: "wrong" },
+							validAttributes: ["name", "description"]
+						}
+					});
+					var model = new Klass({name: "Test", description: "Foo", id: 123});
+					model.description = "Foo changed";
+					var expected = '{"right":{"name":"Test","description":"Foo changed","id":123}}';
+					var actual = model.serialize({changedAttributesOnly: false, rootElement: "right"});
+
+					expect(actual).toEqual(expected);
+				});
+
+			});
+
+		});
+
+	});
+
+});
