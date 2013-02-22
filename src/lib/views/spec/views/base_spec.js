4cbf457bd7fff60df7a253854964754d90476b6b
Greg Burghardt <greg_burghardt@yahoo.com>
2013-02-21 20:59:25 -0600

    Removing invalid spec

--- src/lib/views/spec/views/base_spec.js
+++ src/lib/views/spec/views/base_spec.js
@@ -1,8 +1,5 @@
-describe("BaseView", function() {
-	describe("generateNodeId", function() {
-		it("should generate an Id when a node does not have one", function() {
-			var expectedIds = ["anonymous-node-0", "anonymous-node-1"];
-			expect([BaseView.generateNodeId(), BaseView.generateNodeId()].join(",")).toEqual(expectedIds.join(","));
-		});
+describe("Views", function() {
+	describe("Base", function() {
+		xit("should be tested");
 	});
 });
