44da85b4f077c7fa7d3f9cefb78e16e7efeb631f
Greg Burghardt <greg_burghardt@yahoo.com>
2013-02-21 20:50:14 -0600

    Fixing specs

--- src/lib/modules/spec/modules/factory_spec.js
+++ src/lib/modules/spec/modules/factory_spec.js
@@ -1,224 +1,228 @@
-describe("Modules.Factory", function() {
+describe("Modules", function() {
 
-	window.ModuleFactoryTest = window.ModuleFactoryTest || {};
-	var namespace = window.ModuleFactoryTest;
+	describe("Factory", function() {
 
-	describe("getInstance", function() {
+		window.ModuleFactoryTest = window.ModuleFactoryTest || {};
+		var namespace = window.ModuleFactoryTest;
 
-		ModuleFactoryTest.TestModule = Modules.Base.extend();
+		describe("getInstance", function() {
 
-		beforeEach(function() {
-			this.factory = new Modules.Factory();
+			ModuleFactoryTest.TestModule = Modules.Base.extend();
+
+			beforeEach(function() {
+				this.factory = new Modules.Factory();
+			});
+
+			it("instantiates a module based on the class and registers it with the factory", function() {
+				spyOn(this.factory, "registerModule").andCallThrough();
+				var element = document.createElement("div");
+				var className = "ModuleFactoryTest.TestModule";
+				var module = this.factory.getInstance(className, element);
+
+				expect( module.element.className.replace(/(^\s+)|(\s+$)/g, "") ).toEqual("module module-module_factory_test-test_module");
+				expect(this.factory.registerModule).wasCalledWith(className, module);
+				expect(this.factory.modules[className]).toBeArray();
+				expect(this.factory.modules[className].length).toEqual(1);
+				expect(this.factory.modules[className][0]).toStrictlyEqual(module);
+			});
+
 		});
 
-		it("instantiates a module based on the class and registers it with the factory", function() {
-			spyOn(this.factory, "registerModule").andCallThrough();
-			var element = document.createElement("div");
-			var className = "ModuleFactoryTest.TestModule";
-			var module = this.factory.getInstance(className, element);
+		describe("createModule", function() {
 
-			expect(module.element.className).toEqual("module module-module_factory_test-test_module");
-			expect(this.factory.registerModule).wasCalledWith(className, module);
-			expect(this.factory.modules[className]).toBeArray();
-			expect(this.factory.modules[className].length).toEqual(1);
-			expect(this.factory.modules[className][0]).toStrictlyEqual(module);
-		});
+			ModuleFactoryTest.Foo = Modules.Base.extend();
 
-	});
+			beforeEach(function() {
+				this.factory = new Modules.Factory();
+			});
 
-	describe("createModule", function() {
+			it("calls init() on the new module", function() {
+				var element = document.createElement("div");
+				var moduleInfo = {};
+				var mockModule = new ModuleFactoryTest.Foo(element, moduleInfo.options);
 
-		ModuleFactoryTest.Foo = Modules.Base.extend();
+				spyOn(this.factory, "getInstance").andReturn(mockModule);
+				spyOn(mockModule, "init").andCallThrough();
 
-		beforeEach(function() {
-			this.factory = new Modules.Factory();
-		});
+				this.factory.createModule("ModuleFactoryTest.Foo", element, moduleInfo);
 
-		it("calls init() on the new module", function() {
-			var element = document.createElement("div");
-			var moduleInfo = {};
-			var mockModule = new ModuleFactoryTest.Foo(element, moduleInfo.options);
+				expect(this.factory.getInstance).wasCalled();
+				expect(mockModule.init).wasCalled();
+			});
 
-			spyOn(this.factory, "getInstance").andReturn(mockModule);
-			spyOn(mockModule, "init").andCallThrough();
+			it("prepends modules to the <body> by default", function() {
+				var element = document.createElement("div");
+				var moduleInfo = {};
+				var module = this.factory.createModule("ModuleFactoryTest.Foo", element, moduleInfo);
 
-			this.factory.createModule("ModuleFactoryTest.Foo", element, moduleInfo);
+				expect(module.element.nodeName).toEqual("DIV");
+				expect(module.element).toStrictlyEqual(element);
+				expect(module.element.parentNode.nodeName).toEqual("BODY");
+				expect(document.body.firstChild).toStrictlyEqual(module.element);
+				module.destructor();
+			});
 
-			expect(this.factory.getInstance).wasCalled();
-			expect(mockModule.init).wasCalled();
-		});
-
-		it("prepends modules to the <body> by default", function() {
-			var element = document.createElement("div");
-			var moduleInfo = {};
-			var module = this.factory.createModule("ModuleFactoryTest.Foo", element, moduleInfo);
-
-			expect(module.element.nodeName).toEqual("DIV");
-			expect(module.element).toStrictlyEqual(element);
-			expect(module.element.parentNode.nodeName).toEqual("BODY");
-			expect(document.body.firstChild).toStrictlyEqual(module.element);
-			module.destructor();
-		});
-
-		it("appends modules to the bottom of the <body>", function() {
-			var element = document.createElement("div");
-			var moduleInfo = {
-				insert: "bottom"
-			};
-			var module = this.factory.createModule("ModuleFactoryTest.Foo", element, moduleInfo);
-
-			expect(module.element.nodeName).toEqual("DIV");
-			expect(module.element).toStrictlyEqual(element);
-			expect(module.element.parentNode.nodeName).toEqual("BODY");
-			expect(document.body.lastChild).toStrictlyEqual(module.element);
-			module.destructor();
-		});
-
-		it("appends modules based on a CSS selector", function() {
-			var containerElement = document.createElement("section");
-			containerElement.id = "__test__module_factory_container";
-			containerElement.innerHTML = '<p>I am a teapot</p>';
-			document.body.appendChild(containerElement);
-
-			var element = document.createElement("div");
-			var moduleInfo = {
-				container: "section#__test__module_factory_container",
-				insert: "bottom"
-			};
-			var module = this.factory.createModule("ModuleFactoryTest.Foo", element, moduleInfo);
-
-			expect(module.element.parentNode).toStrictlyEqual(containerElement);
-			expect(containerElement.lastChild).toStrictlyEqual(module.element);
-			module.destructor();
-			containerElement.parentNode.removeChild(containerElement);
-		});
-
-		it("prepends modules based on a CSS selector", function() {
-			var containerElement = document.createElement("section");
-			containerElement.id = "__test__module_factory_container";
-			containerElement.innerHTML = '<p>I am a teapot</p>';
-			document.body.appendChild(containerElement);
-
-			var element = document.createElement("div");
-			var moduleInfo = {
-				container: "section#__test__module_factory_container"
-			};
-			var module = this.factory.createModule("ModuleFactoryTest.Foo", element, moduleInfo);
-
-			expect(module.element.parentNode).toStrictlyEqual(containerElement);
-			expect(containerElement.firstChild).toStrictlyEqual(module.element);
-			module.destructor();
-			containerElement.parentNode.removeChild(containerElement);
-		});
-
-		it("throws an error when the container selector returns no element", function() {
-			var err;
-			var element = document.createElement("div");
-			var moduleInfo = {
-				container: "#non_existent_id"
-			};
-			var module;
-
-			try {
-				module = this.factory.createModule("ModuleFactoryTest.Foo", element, moduleInfo);
-			}
-			catch (error) {
-				err = error;
-			}
-
-			expect(err).toBeInstanceof(Error);
-			expect( /Could not find module container/.test(err.message) ).toBeTrue();
-		});
-
-		it("creates the module element as any element", function() {
-			var element = document.createElement("div");
-			var moduleInfo = {
-				element: "section"
-			};
-			var module = this.factory.createModule("ModuleFactoryTest.Foo", element, moduleInfo);
-
-			expect(module.element.nodeName).toEqual("SECTION");
-			module.destructor();
-		});
-
-		it("configures the new module with options", function() {
-			var element = document.createElement("div");
-			var moduleInfo = {
-				options: {
-					id: 123
-				}
-			};
-			var module = this.factory.createModule("ModuleFactoryTest.Foo", element, moduleInfo);
-
-			expect(module.options.id).toEqual(123);
-		});
-
-	});
-
-	describe("createModules", function() {
-
-		beforeEach(function() {
-			this.element = document.createElement("div");
-			this.factory = new Modules.Factory();
-		});
-
-		ModuleFactoryTest.Foo = Modules.Base.extend();
-		ModuleFactoryTest.Bar = Modules.Base.extend();
-
-		it("creates a single module", function() {
-			var moduleInfo = {
-				options: { id: 1 }
-			};
-
-			this.element.setAttribute("data-module", "ModuleFactoryTest.Foo");
-			this.element.setAttribute("data-module-info", JSON.stringify(moduleInfo));
-
-			var modules = this.factory.createModules(this.element);
-
-			expect(modules.length).toEqual(1);
-			expect(modules[0]).toBeInstanceof(ModuleFactoryTest.Foo);
-			expect(modules[0].options.id).toEqual(1);
-		});
-
-		describe("creating modules in bulk", function() {
-
-			it("gets module info based on the class name", function() {
+			it("appends modules to the bottom of the <body>", function() {
+				var element = document.createElement("div");
 				var moduleInfo = {
-					"ModuleFactoryTest.Foo": {
-						options: { id: 1 }
-					},
-					"ModuleFactoryTest.Bar": {
-						options: { id: 2 }
+					insert: "bottom"
+				};
+				var module = this.factory.createModule("ModuleFactoryTest.Foo", element, moduleInfo);
+
+				expect(module.element.nodeName).toEqual("DIV");
+				expect(module.element).toStrictlyEqual(element);
+				expect(module.element.parentNode.nodeName).toEqual("BODY");
+				expect(document.body.lastChild).toStrictlyEqual(module.element);
+				module.destructor();
+			});
+
+			it("appends modules based on a CSS selector", function() {
+				var containerElement = document.createElement("section");
+				containerElement.id = "__test__module_factory_container";
+				containerElement.innerHTML = '<p>I am a teapot</p>';
+				document.body.appendChild(containerElement);
+
+				var element = document.createElement("div");
+				var moduleInfo = {
+					container: "section#__test__module_factory_container",
+					insert: "bottom"
+				};
+				var module = this.factory.createModule("ModuleFactoryTest.Foo", element, moduleInfo);
+
+				expect(module.element.parentNode).toStrictlyEqual(containerElement);
+				expect(containerElement.lastChild).toStrictlyEqual(module.element);
+				module.destructor();
+				containerElement.parentNode.removeChild(containerElement);
+			});
+
+			it("prepends modules based on a CSS selector", function() {
+				var containerElement = document.createElement("section");
+				containerElement.id = "__test__module_factory_container";
+				containerElement.innerHTML = '<p>I am a teapot</p>';
+				document.body.appendChild(containerElement);
+
+				var element = document.createElement("div");
+				var moduleInfo = {
+					container: "section#__test__module_factory_container"
+				};
+				var module = this.factory.createModule("ModuleFactoryTest.Foo", element, moduleInfo);
+
+				expect(module.element.parentNode).toStrictlyEqual(containerElement);
+				expect(containerElement.firstChild).toStrictlyEqual(module.element);
+				module.destructor();
+				containerElement.parentNode.removeChild(containerElement);
+			});
+
+			it("throws an error when the container selector returns no element", function() {
+				var err;
+				var element = document.createElement("div");
+				var moduleInfo = {
+					container: "#non_existent_id"
+				};
+				var module;
+
+				try {
+					module = this.factory.createModule("ModuleFactoryTest.Foo", element, moduleInfo);
+				}
+				catch (error) {
+					err = error;
+				}
+
+				expect(err).toBeInstanceof(Error);
+				expect( /Could not find module container/.test(err.message) ).toBeTrue();
+			});
+
+			it("creates the module element as any element", function() {
+				var element = document.createElement("div");
+				var moduleInfo = {
+					element: "section"
+				};
+				var module = this.factory.createModule("ModuleFactoryTest.Foo", element, moduleInfo);
+
+				expect(module.element.nodeName).toEqual("SECTION");
+				module.destructor();
+			});
+
+			it("configures the new module with options", function() {
+				var element = document.createElement("div");
+				var moduleInfo = {
+					options: {
+						id: 123
 					}
 				};
+				var module = this.factory.createModule("ModuleFactoryTest.Foo", element, moduleInfo);
 
-				this.element.setAttribute("data-module", "ModuleFactoryTest.Foo ModuleFactoryTest.Bar");
+				expect(module.options.id).toEqual(123);
+			});
+
+		});
+
+		describe("createModules", function() {
+
+			beforeEach(function() {
+				this.element = document.createElement("div");
+				this.factory = new Modules.Factory();
+			});
+
+			ModuleFactoryTest.Foo = Modules.Base.extend();
+			ModuleFactoryTest.Bar = Modules.Base.extend();
+
+			it("creates a single module", function() {
+				var moduleInfo = {
+					options: { id: 1 }
+				};
+
+				this.element.setAttribute("data-module", "ModuleFactoryTest.Foo");
 				this.element.setAttribute("data-module-info", JSON.stringify(moduleInfo));
 
 				var modules = this.factory.createModules(this.element);
 
-				expect(modules.length).toEqual(2);
+				expect(modules.length).toEqual(1);
 				expect(modules[0]).toBeInstanceof(ModuleFactoryTest.Foo);
 				expect(modules[0].options.id).toEqual(1);
-				expect(modules[1]).toBeInstanceof(ModuleFactoryTest.Bar);
-				expect(modules[1].options.id).toEqual(2);
-
-				modules[0].destructor();
-				modules[1].destructor();
 			});
 
-			it("does not require module info specific to a class", function() {
-				this.element.setAttribute("data-module", "ModuleFactoryTest.Foo ModuleFactoryTest.Bar");
-				this.element.setAttribute("data-module-info", "{}");
+			describe("creating modules in bulk", function() {
 
-				var modules = this.factory.createModules(this.element);
+				it("gets module info based on the class name", function() {
+					var moduleInfo = {
+						"ModuleFactoryTest.Foo": {
+							options: { id: 1 }
+						},
+						"ModuleFactoryTest.Bar": {
+							options: { id: 2 }
+						}
+					};
 
-				expect(modules.length).toEqual(2);
-				expect(modules[0]).toBeInstanceof(ModuleFactoryTest.Foo);
-				expect(modules[1]).toBeInstanceof(ModuleFactoryTest.Bar);
+					this.element.setAttribute("data-module", "ModuleFactoryTest.Foo ModuleFactoryTest.Bar");
+					this.element.setAttribute("data-module-info", JSON.stringify(moduleInfo));
 
-				modules[0].destructor();
-				modules[1].destructor();
+					var modules = this.factory.createModules(this.element);
+
+					expect(modules.length).toEqual(2);
+					expect(modules[0]).toBeInstanceof(ModuleFactoryTest.Foo);
+					expect(modules[0].options.id).toEqual(1);
+					expect(modules[1]).toBeInstanceof(ModuleFactoryTest.Bar);
+					expect(modules[1].options.id).toEqual(2);
+
+					modules[0].destructor();
+					modules[1].destructor();
+				});
+
+				it("does not require module info specific to a class", function() {
+					this.element.setAttribute("data-module", "ModuleFactoryTest.Foo ModuleFactoryTest.Bar");
+					this.element.setAttribute("data-module-info", "{}");
+
+					var modules = this.factory.createModules(this.element);
+
+					expect(modules.length).toEqual(2);
+					expect(modules[0]).toBeInstanceof(ModuleFactoryTest.Foo);
+					expect(modules[1]).toBeInstanceof(ModuleFactoryTest.Bar);
+
+					modules[0].destructor();
+					modules[1].destructor();
+				});
+
 			});
 
 		});
